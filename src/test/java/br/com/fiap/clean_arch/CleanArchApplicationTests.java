6ce7eb89779aaac46919f869fcb21bcd9e15768d
MaykPlatero <maykp.platero1910@gmail.com>
2025-12-05 18:03:37 -0300

    Ajustando o pom.xml para compilação em linux

--- src/test/java/br/com/fiap/clean_arch/CleanArchApplicationTests.java
+++ src/test/java/br/com/fiap/clean_arch/CleanArchApplicationTests.java
@@ -2,10 +2,14 @@ package br.com.fiap.clean_arch;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@TestPropertySource(properties = {
+	"spring.datasource.url=jdbc:h2:mem:testdb",
+	"spring.datasource.driver-class-name=org.h2.Driver",
+	"spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class CleanArchApplicationTests {
 
 	@Test
