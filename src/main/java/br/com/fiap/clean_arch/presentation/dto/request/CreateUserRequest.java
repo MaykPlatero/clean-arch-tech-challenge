3f5a8bfb5aa9fbe85247070e4b69bb3c0509c61a
carlosferreira <art97496@gmail.com>
2026-01-15 16:59:39 -0300

    aplicando validação  updateUser e adicionando politicas do name

--- src/main/java/br/com/fiap/clean_arch/presentation/dto/request/CreateUserRequest.java
+++ src/main/java/br/com/fiap/clean_arch/presentation/dto/request/CreateUserRequest.java
@@ -5,20 +5,23 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 
 public record CreateUserRequest(
-        @NotBlank(message = "Name é obrigatorio")
+        @NotBlank(message = "Name are required")
+        @Pattern(regexp = "^[A-Za-zÀ-ÿ ]+$", message = "Nome deve conter apenas letras")
         String name,
-        @NotBlank(message = "Email é Obrigatorio")
-        @Email(message = "Email deve ser válido")
+        @NotBlank(message = "Email are required")
+        @Email(message = "Email must be valid")
         String email,
         @NotNull(message = "User IDs are required")
         String userIdentification,
         @NotBlank(message = "Address is required")
         String address,
+        @NotBlank(message = "profile are required")
         String profile,
-        @NotBlank(message = "nick name é obrigatorio")
+        @NotBlank(message = "nick are required")
         String username,
-        @NotBlank(message = "campo senha é obrigatorio")
-        @Size(min = 6, message = "Senha deve ter no minimo 6 caracteres")
+        @NotBlank(message = "senha are required")
+        @Size(min = 6, message = "Password must be at least 6 characters long")
         String password) {}
